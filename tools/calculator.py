12e50d38872aff9fb5ea3ad0677d1d3e0bb2884e
behlah-rgb <behlahkathavala@gmail.com>
2026-07-05 18:58:04 +0530

    Refactored calculator using functions

--- tools/calculator.py
+++ tools/calculator.py
@@ -5,6 +5,41 @@
 # Exponent
 # Factorial
 
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a-b
+
+
+def multiply(a, b):
+    return a*b
+
+
+def divide(a, b):
+    if b == 0:
+        return "Divisor can't be Zero! Try Again."
+    else:
+        return a/b
+
+
+def exponent(a, b):
+    return a**b
+
+
+def factorial(n):
+    if n < 0:
+        return "Not valid for negative numbers"
+    elif n == 0:
+        return "Factorial: 1"
+    else:
+        fact = n
+        for i in range(2, n):
+            fact *= i
+        return f"Factorial: {fact}"
+
+
 while True:
     choice = int(input('''Enter the your choice:
     1] Addition
@@ -13,50 +48,38 @@ while True:
     4] Division
     5] Exponent
     6] Factorial
+    7] Quit
     '''))
     match choice:
         case 1:
             augend = int(input("Enter the augend: "))
             addend = int(input("Enter the addend: "))
-            sum = augend + addend
+            sum = add(augend, addend)
             print('Sum: ', sum)
-            break
         case 2:
             minuend = int(input("Enter the minuend: "))
             subtrahend = int(input("Enter the subtrahend: "))
-            diff = minuend - subtrahend
+            diff = subtract(minuend, subtrahend)
             print('Difference: ', diff)
-            break
         case 3:
             multiplicand = int(input("Enter the multiplicand: "))
             multiplier = int(input("Enter the multiplier: "))
-            prod = multiplicand*multiplier
+            prod = multiply(multiplicand, multiplier)
             print('Product: ', prod)
-            break
         case 4:
             dividend = int(input("Enter the dividend: "))
             divisor = int(input("Enter the divisor: "))
-            quotient = dividend/divisor
+            quotient = divide(dividend, divisor)
             print('Quotient: ', quotient)
-            break
         case 5:
             base = int(input("Enter the base: "))
             exp = int(input("Enter the exponent: "))
-            power = base**exp
+            power = exponent(base, exp)
             print('Power: ', power)
-            break
         case 6:
             n = int(input("Enter the number: "))
-            if n < 0 :
-                print("Not valid for negative numbers")
-            elif n == 0:
-                print("Factorial: 1")
-                break
-            else: 
-                fact = n
-                for i in range(2, n):
-                    fact *= i
-                print("Factorial: ", fact)
-                break
+            print(factorial(n))
+        case 7:
+            break
         case _:
             print("Invalid choice try again!")
